82cab6bda00924cb05c7776950663af9a95527b8
Thomas Nägele <hello.thomas.naegele@gmail.com>
2017-02-26 13:39:18 +0100

    - use custom fields in page-gastronomie.php

--- page-gastronomie.php
+++ page-gastronomie.php
@@ -4,8 +4,54 @@
 */
 
 get_header() ?>
-
-<h2 class="h2">
-  <? echo "page-gastronomie.php"; ?>
-</h2>
+<section class="hero">
+  <div class="wrapper">
+    <h1 class="hero__h1">
+      <? echo get_field('page_title'); ?>
+    </h1>
+    <h2 class="hero__h2">
+      <? echo get_field('page_subtitle'); ?>
+    </h2>
+  </div>
+</section>
+<section class="schankanlagen section">
+  <div class="wrapper">
+    <div class="section__titles">
+      <h2 class="section__title">
+        <? echo get_field('section_1_title'); ?>
+      </h2>
+      <h3 class="section__subtitle">
+        <? echo get_field('section_1_subtitle'); ?>
+      </h3>
+    </div>
+    <div class="section__grid schankanlagen__grid">
+      <div class="schankanlagen__img section__img">
+        <img src="<? the_field('section_1_image'); ?>">
+      </div>
+      <div class="schankanlagen__content section__content">
+        <? echo get_field('section_1_text'); ?>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="festservice section section--alt">
+  <div class="wrapper">
+    <div class="section__titles">
+      <h2 class="section__title">
+        <? echo get_field('section_2_title'); ?>
+      </h2>
+      <h3 class="section__subtitle">
+        <? echo get_field('section_2_subtitle'); ?>
+      </h3>
+    </div>
+    <div class="section__grid festservice__grid">
+      <div class="festservice__img section__img">
+        <img src="<? the_field('section_2_image'); ?>">
+      </div>
+      <div class="festservice__content section__content">
+        <? echo get_field('section_2_text'); ?>
+      </div>
+    </div>
+  </div>
+</section>
 <? get_footer() ?>
