8dd9335a2e3d52afbc46e93b3f54c9f5b9500fd4
Thomas Nägele <hello.thomas.naegele@gmail.com>
2017-03-25 16:27:40 +0100

    - add related products to single-product.php

--- functions.php
+++ functions.php
@@ -1,5 +1,75 @@
 <?php
 
+function get_related_posts( $taxonomy = '', $args = [] )
+{
+    /*
+     * Before we do anything and waste unnecessary time and resources, first check if we are on a single post page
+     * If not, bail early and return false
+     */
+    if ( !is_single() )
+        return false;
+
+    /*
+     * Check if we have a valid taxonomy and also if the taxonomy exists to avoid bugs further down.
+     * Return false if taxonomy is invalid or does not exist
+     */
+    if ( !$taxonomy )
+        return false;
+
+    $taxonomy = filter_var( $taxonomy, FILTER_SANITIZE_STRING );
+    if ( !taxonomy_exists( $taxonomy ) )
+        return false;
+
+    /*
+     * We have made it to here, so we should start getting our stuff togther.
+     * Get the current post object to start of
+     */
+    $current_post = get_queried_object();
+
+    /*
+     * Get the post terms, just the ids
+     */
+    $terms = wp_get_post_terms( $current_post->ID, $taxonomy, ['fields' => 'ids'] );
+
+    /*
+     * Lets only continue if we actually have post terms and if we don't have an WP_Error object. If not, return false
+     */
+    if ( !$terms || is_wp_error( $terms ) )
+        return false;
+
+    /*
+     * Set the default query arguments
+     */
+    $defaults = [
+        'post_type' => $current_post->post_type,
+        'post__not_in' => [$current_post->ID],
+        'orderby' => 'rand',
+        'tax_query' => [
+            [
+                'taxonomy' => $taxonomy,
+                'terms' => $terms,
+                'include_children' => false
+            ],
+        ],
+    ];
+
+    /*
+     * Validate and merge the defaults with the user passed arguments
+     */
+    if ( is_array( $args ) ) {
+        $args = wp_parse_args( $args, $defaults );
+    } else {
+        $args = $defaults;
+    }
+
+    /*
+     * Now we can query our related posts and return them
+     */
+    $q = get_posts( $args );
+
+    return $q;
+}
+
 // Add support for product thumbnails
 add_theme_support( 'post-thumbnails' );
 
--- single-product.php
+++ single-product.php
@@ -14,24 +14,64 @@
       <h1 class="hero__h1"><? the_title(); ?></h1>
       <h2 class="hero__h2">
         <?
-         echo get_the_terms($post->ID, 'product_category')[0]->name;
+         $category = get_the_terms($post->ID, 'product_category')[0]->name;
+         echo $category;
         ?>
       </h2>
     </div>
   </div>
   <div class="wrapper">
-    <div class="grid--single-prod">
-      <div class="product__image">
-        <? if (has_post_thumbnail()) :
-             the_post_thumbnail('full');
-           endif;
+    <section class="section--single-prod section">
+      <div class="grid--single-prod">
+        <div class="product__image">
+          <? if (has_post_thumbnail()) :
+               the_post_thumbnail('full');
+             endif;
+          ?>
+        </div>
+        <div class="product__content">
+          <p><? the_field('product_description') ?></p>
+          <p><? the_field('product_quantity') ?> <? echo __('Liter Flasche'); ?></p>
+        </div>
+      </div>
+    </section>
+    <section class="related">
+      <h4 class="related__title"><? echo __('Weitere Produkte'); ?></h4>
+      <div class="products">
+        <?
+        if ( function_exists( 'get_related_posts' ) ) {
+            $related_posts = get_related_posts( 'product_category', ['posts_per_page' => 8] );
+            if ( $related_posts ) {
+                foreach ( $related_posts as $post ) {
+                    setup_postdata( $post );
+                    ?>
+
+                    <article class="product <? echo $post->ID; ?>">
+                      <a href="<? echo get_permalink(); ?>" class="product__link" title="<? echo __('Produkt anzeigen'); ?>">
+                        <?
+                          if ( has_post_thumbnail() ) {
+                            the_post_thumbnail('product-thumb');
+                          }
+                        ?>
+                        <h3 class="product__category">
+                          <span>
+                            <? echo get_the_terms($post->ID, 'product_category')[0]->name; ?>
+                          </span>
+                          <span class="product__quantity">
+                            <? echo get_field('product_quantity') ?>&nbsp;<span class="text--small">L</span>
+                          </span>
+                        </h3>
+                        <h4 class="product__name"><? echo $post->post_title; ?></h4>
+                      </a>
+                    </article>
+                <?
+                }
+                wp_reset_postdata();
+            }
+        }
         ?>
       </div>
-      <div class="product__content">
-        <p><? the_field('product_description') ?></p>
-        <p><? the_field('product_quantity') ?> <? echo __('Liter Flasche'); ?></p>
-      </div>
-    </div>
+    </section>
   </div>
 
 <? endif; ?>
